7c043cf113aede60b11392bff402e053762dbdc7
zsh <43240825@qq.com>
2019-01-12 15:30:49 +0800

    实体

---
+++ src/main/java/org/wlgzs/index_evaluation/pojo/year.java
@@ -0,0 +1,18 @@
+package org.wlgzs.index_evaluation.pojo;
+
+/**
+ * @author zsh
+ * @company wlgzs
+ * @create 2019-01-12 15:24
+ * @Describe 年份实体类
+ */
+public class year {
+    /**
+     * 年份id
+     */
+    private Integer yearId;
+    /**
+     * 年份
+     */
+    private Integer yearName;
+}
