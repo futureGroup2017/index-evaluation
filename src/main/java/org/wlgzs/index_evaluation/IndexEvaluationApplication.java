0ee9fbcd3a1aa64ee02a22a5ef3c4f9233903ef8
zsh <43240825@qq.com>
2019-01-12 12:18:24 +0800

    e

--- src/main/java/org/wlgzs/index_evaluation/IndexEvaluationApplication.java
+++ src/main/java/org/wlgzs/index_evaluation/IndexEvaluationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IndexEvaluationApplication extends ServletInitializer{
+public class IndexEvaluationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IndexEvaluationApplication.class, args);
