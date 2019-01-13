5e775630556c422fafb08f11a5137833d47ec5f8
wky181 <1813721268@qq.com>
2019-01-13 17:33:53 +0800

    专业crud已完毕

---
+++ src/main/java/org/wlgzs/index_evaluation/pojo/Major.java
@@ -0,0 +1,26 @@
+package org.wlgzs.index_evaluation.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 武凯焱
+ * @date 2019/1/13 10:59
+ * @Description:
+ */
+@Data
+@NoArgsConstructor
+@TableName("tb_major")
+public class Major {
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "major_id", type = IdType.AUTO)
+    private Integer maiorId;
+    @TableField("major_name")
+    private String majorName;
+}
